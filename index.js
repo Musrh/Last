bc294f623518facabf97bf89c14e3f0be9d7a85e
Musrh <musplay2023@gmail.com>
2026-02-26 04:55:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-// run `node index.js` in the terminal
+const mongoose = require("mongoose");
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// 🔹 Connexion MongoDB
+const mongoURL = process.env.MONGO_URL;
+if (!mongoURL) {
+  console.error("⚠️ MONGO_URL n'est pas défini !");
+  process.exit(1);
+}
+
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connecté ✅"))
+  .catch(err => console.log("Erreur MongoDB ❌ :", err));
